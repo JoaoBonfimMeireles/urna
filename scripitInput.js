21546646959b4e0ac15956e605cd0752cdf18289
JoaoBonfimMeireles <jpwmeireles44@gmail.com>
2024-03-20 14:11:29 -0300

    crei o input variavel e pegar esse valor como number | deixei responsivo

---
+++ scripitInput.js
@@ -0,0 +1,21 @@
+const numberOfInputs = 5 
+
+const inputContainer = document.getElementById("inputContainer");
+
+const inputValues = []
+
+for(i = 0; i < numberOfInputs; i ++) {
+    const input = document.createElement("input");
+    input.type = "text"
+    input.maxLength = 1
+    input.value = ""
+    input.id = "input" + i
+    inputContainer.appendChild(input)
+}
+
+for (let i = 0; i < numberOfInputs; i++) {
+    const inputValue = document.getElementById("input" + i).value;
+    inputValues.push(Number(inputValue));
+}
+
+console.log(inputValues);
